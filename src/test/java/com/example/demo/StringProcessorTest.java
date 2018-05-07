ef228fbb59b3d5223f525668ae741951c0a3f677
Akthar Sirajudheen and Clayton Yarborough <akthar_sirajudheen+clayton_yarborough@homedepot.com>
2018-05-07 16:37:59 -0400

    test refactor

--- src/test/java/com/example/demo/StringProcessorTest.java
+++ src/test/java/com/example/demo/StringProcessorTest.java
@@ -2,15 +2,23 @@ package com.example.demo;
 
 import com.example.demo.dto.StringProcessor;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.logging.Logger;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class StringProcessorTest {
 
     StringProcessor stringProcessor = new StringProcessor();
+    Logger logger = Logger.getLogger(StringProcessorTest.class.getName());
 
     @Test
     public void testIsPalyndromeSuccessWithNull(){
+        logger.info("Running...");
         Assert.assertEquals(true,stringProcessor.isPalyndrome(null));
     }
 
