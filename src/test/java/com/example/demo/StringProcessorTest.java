d3b2cc8ccea5003efd2bc53173893009d7802541
Akthar Sirajudheen and Clayton Yarborough <akthar_sirajudheen+clayton_yarborough@homedepot.com>
2018-05-08 09:53:21 -0400

    Added applicationcontext importing in the test class

--- src/test/java/com/example/demo/StringProcessorTest.java
+++ src/test/java/com/example/demo/StringProcessorTest.java
@@ -5,12 +5,14 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.logging.Logger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ImportResource({"classpath*:applicationContext.xml"})
 public class StringProcessorTest {
 
     StringProcessor stringProcessor = new StringProcessor();
@@ -18,22 +20,25 @@ public class StringProcessorTest {
 
     @Test
     public void testIsPalyndromeSuccessWithNull(){
-        logger.info("Running...");
+        logger.info("Running.......................................................1");
         Assert.assertEquals(true,stringProcessor.isPalyndrome(null));
     }
 
     @Test
     public void testIsPalyndromeSuccessWithSingleLetter(){
+        logger.info("Running.......................................................2");
         Assert.assertEquals(true,stringProcessor.isPalyndrome("a"));
     }
 
     @Test
     public void testIsPalyndromeSuccess(){
+        logger.info("Running.......................................................3");
         Assert.assertEquals(true,stringProcessor.isPalyndrome("abba"));
     }
 
     @Test
     public void testIsPalyndromeReturnsFalse(){
+        logger.info("Running.......................................................4");
         Assert.assertEquals(false,stringProcessor.isPalyndrome("abbae"));
     }
 }
