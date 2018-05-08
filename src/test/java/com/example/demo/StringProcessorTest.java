9cec2e71666f8c71c330de254d7083d27211ff65
Akthar Sirajudheen and Clayton Yarborough <akthar_sirajudheen+clayton_yarborough@homedepot.com>
2018-05-08 11:28:35 -0400

    Cleaned the application

--- src/test/java/com/example/demo/StringProcessorTest.java
+++ src/test/java/com/example/demo/StringProcessorTest.java
@@ -3,20 +3,13 @@ package com.example.demo;
 import com.example.demo.dto.StringProcessor;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.logging.Logger;
 
-//@RunWith(SpringRunner.class)
 @SpringBootTest
-//@ImportResource({"classpath*:applicationContext.xml"})
 public class StringProcessorTest {
 
-//    @Autowired
     StringProcessor stringProcessor = new StringProcessor();
     Logger logger = Logger.getLogger(StringProcessorTest.class.getName());
 
